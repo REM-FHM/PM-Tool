4dde6b53ad639069b61e0a2cc08d70807ee2504d
HansWurst90 <jan@mussenbrock-w.de>
2015-12-11 21:26:28 +0100

    Seed.rb für PBS-Beispieldaten angepasst.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,20 @@
 puts "Seed wird eingelesen"
 
 
-Project.create(name: 'Web-Shop', id: 1)
-ProductBreakdownStructure.
-
+Project.create(id: 1, name: 'Web-Shop')
+ProductBreakdownStructure.create(id: 1, p_id: 1)
+Subproduct.create(id: 1, name: 'Geschäftsplan und Architektur', pbs_id: 1)
+Modul.create(id: 1, name: 'Geschäftsplan', subproduct_id: 1)
+Component.create(id: 1, name: 'Business Case', modul_id: 1)
+Component.create(id: 2, name: 'Projektplan', modul_id: 1)
+Component.create(id: 3, name: 'Lastenheft', modul_id: 1)
+Modul.create(id: 2, name: 'Architektur', subproduct_id: 1)
+Component.create(id: 4, name: 'Anwendungsarchitektur', modul_id: 2)
+Component.create(id: 5, name: 'Technische Architektur', modul_id: 2)
+Subproduct.create(id: 2, name: 'Geschäftsplan und Architektur', pbs_id: 1)
+Modul.create(id: 3, name: 'Kunden- und Produktverwaltung', subproduct_id: 2)
+Component.create(id: 6, name: 'Kundenstammdaten', modul_id: 3)
+Component.create(id: 7, name: 'Kundenbewegungsdaten', modul_id: 3)
 
 
 puts "Seed abgeschlossen!"
