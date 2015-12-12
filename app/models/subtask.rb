8fc5a1c82e3182ff3d9d2170e57599cf819ab415
PiCookie <l.erfkaemper@gmx.de>
2015-12-12 14:55:56 +0100

    Seeds erweitert

--- app/models/subtask.rb
+++ app/models/subtask.rb
@@ -1,2 +1,4 @@
 class Subtask < ActiveRecord::Base
+	belongs_to :task
+	has_many :workpackages, dependent: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-puts "Seed wird eingelesen"
+puts "Seed intialisiert"
 
 
 Project.create(id: 1, name: 'Web-Shop')
+
+puts "ProductBreakdownStructure wird eingelesen"
 ProductBreakdownStructure.create(id: 1, p_id: 1)
 Subproduct.create(id: 1, name: 'Geschäftsplan und Architektur', pbs_id: 1)
 Modul.create(id: 1, name: 'Geschäftsplan', subproduct_id: 1)
@@ -23,5 +25,18 @@ Modul.create(id: 3, name: 'Kundendatenbank', subproduct_id: 2)
 Component.create(id: 6, name: 'Kundenstammdaten', modul_id: 3)
 Component.create(id: 7, name: 'Kundenbewegungsdaten', modul_id: 3)
 
+puts "WorkBreakdownStructure wird eingelesen"
+WorkBreakdownStructure.create(id: 1, p_id: 1)
+Task.create(id: 1, name: 'Projektmanagement', wbs_id: 1)
+Subtask.create(id: 1, name: 'Geschäftsmodellierung', task_id: 1)
+Workpackage.create(id: 1, name: 'Wirtschaftlichkeitsrechnung erstellen', subtask_id: 1)
+Workpackage.create(id: 2, name: 'Lastenheft erstellen', subtask_id: 1)
+Subtask.create(id: 2, name: 'Projektplanung', task_id: 1)
+Workpackage.create(id: 3, name: 'Ablauf- und Terminplanung', subtask_id: 2)
+Workpackage.create(id: 4, name: 'Einsatzmittel- und Kostenplanung', subtask_id: 2)
+Task.create(id: 2, name: 'Definition und Entwurf', wbs_id: 1)
+Subtask.create(id: 3, name: 'Funktionale Architektur', task_id: 2)
+Workpackage.create(id: 5, name: 'Funtionen- und GUI-Modellierung', subtask_id: 3)
+Workpackage.create(id: 6, name: 'Prozess- und Datenmodellierung', subtask_id: 3)
 
 puts "Seed abgeschlossen!"
